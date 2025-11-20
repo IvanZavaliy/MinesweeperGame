9e0df7639ed946ef87f17034985c5c1b003e566f
IvanZavaliy <ivan.zavivan@gmail.com>
2025-11-20 22:52:36 +0200

    Add player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public float stepDistance = 1f;
+    public float moveDuration = 0.25f;
+    private bool isMoving = false;
+
+    void OnEnable()
+    {
+        PlayerInput.OnMoveUp += HandleMoveUp;
+        PlayerInput.OnMoveDown += HandleMoveDown;
+        PlayerInput.OnMoveRight += HandleMoveRight;
+        PlayerInput.OnMoveLeft += HandleMoveLeft;
+    }
+
+    void OnDisable()
+    {
+        PlayerInput.OnMoveUp -= HandleMoveUp;
+        PlayerInput.OnMoveDown -= HandleMoveDown;
+        PlayerInput.OnMoveRight -= HandleMoveRight;
+        PlayerInput.OnMoveLeft -= HandleMoveLeft;
+    }
+
+    void HandleMoveUp()
+    {
+        if (!isMoving) StartCoroutine(MoveStep(Vector3.up * stepDistance));
+    }
+    void HandleMoveDown()
+    {
+        if (!isMoving) StartCoroutine(MoveStep(Vector3.down * stepDistance));
+    }
+    void HandleMoveRight()
+    {
+        if (!isMoving) StartCoroutine(MoveStep(Vector3.right * stepDistance));
+    }
+    void HandleMoveLeft()
+    {
+        if (!isMoving) StartCoroutine(MoveStep(Vector3.left * stepDistance));
+    }
+
+    IEnumerator MoveStep(Vector3 moveVector)
+    {
+        isMoving = true;
+        Vector3 startPos = transform.position;
+        Vector3 endPos = startPos + moveVector;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < moveDuration)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, (elapsedTime / moveDuration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.position = endPos; 
+        isMoving = false;
+    }
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,30 +6,47 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private Minefield minefield;
     [SerializeField] private Player player;
     public bool isActive = true;
+    
+    public static event Action OnMoveUp;
+    public static event Action OnMoveDown;
+    public static event Action OnMoveRight;
+    public static event Action OnMoveLeft;
 
     private void Update()
     {
         if (!isActive) return;
 
-        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
+        if (Input.GetMouseButtonUp(0) && !minefield.GameStarted)
         {
-            InteractWithCell();
+            DigUpStartingCell();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            OnMoveUp?.Invoke();
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            OnMoveDown?.Invoke();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            OnMoveRight?.Invoke();
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            OnMoveLeft?.Invoke();
         }
     }
 
-    private void InteractWithCell()
+    private void DigUpStartingCell()
     {
         RaycastHit2D hit = Utils.GetRaycastHit2DFromMousePosition();
         if (!hit) return;
         
-        Vector2Int coords = new Vector2Int((int)hit.transform.position.x, (int)hit.transform.position.y);
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (!minefield.GameStarted) minefield.GetCoordsForPlayer(coords);
-            minefield.OpenCellByCoords(coords);
-        }
-        else if  (Input.GetMouseButtonUp(1) && minefield.GameStarted)
-            minefield.SetBombFlag(coords);
+        Vector2Int coords = new Vector2Int((int)hit.transform.position.x, (int)hit.transform.position.y); 
+        
+        minefield.GetCoordsForPlayer(coords);
+        minefield.OpenCellByCoords(coords);
     }
 }
